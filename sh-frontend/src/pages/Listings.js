e6afc40a9e1cfd5be1d2d7652d475a1d93aee13b
Lex de Kort <lexdekort@live.nl>
2022-10-02 23:42:13 +0200

    Implement communication between front-end and back-end for listings

--- sh-frontend/src/pages/Listings.js
+++ sh-frontend/src/pages/Listings.js
@@ -1,7 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function Listings(){
+    const [listings, setListings] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/listings").then((response) => {
+            setListings(response.data.listings);
+        });
+    });
+
     return(
         <>
             <h2>This is a listings page.</h2>
+            {listings.map(listing => (
+                <p>{listing.address}</p>
+            ))}
         </>
     )
 }
